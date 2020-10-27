b617307cacceedb0cd39b03696343aa8e09952aa
Marcel <dolly.marcel@gmail.com>
2020-10-26 22:54:34 -0300

    Envelope spider script into a class

--- spider.py
+++ spider.py
@@ -16,51 +16,72 @@ stage_tags = ['BACKLOG',
               'READY TO HOMOLOGATION',
               'HOMOLOGATION']
 
-html = open('/home/marcelbornancin/Downloads/2974.html', 'r')
-soup = BeautifulSoup(html, 'html.parser')
 
-activity_selector = '.timeline-entry .timeline-content .note-headline-light'
+class TaskSpider():
 
-stage_updates = []
+    activity_selector = ('.timeline-entry '
+                         '.timeline-content '
+                         '.note-headline-light ')
 
-for activity in soup.select(activity_selector):
-    activity_message_node = activity.find('span', class_='system-note-message')
+    def __init__(self, task_code):
+        self.task_url = f'/home/marcelbornancin/Downloads/{task_code}.html'
+        self.stage_updates = []
 
-    if activity_message_node is None:
-        continue
+    def scrap_metrics(self):
 
-    contents = [c for c in activity_message_node.find('span').children]
+        html_file = open(self.task_url, 'r')
+        soup = BeautifulSoup(html_file, 'html.parser')
 
-    is_tag_action = len(contents) in [3, 5]
-    if not is_tag_action:
-        continue
+        for activity in soup.select(TaskSpider.activity_selector):
+            activity_message_node = activity.find('span',
+                                                  class_='system-note-message')
+            if activity_message_node is None:
+                continue
 
-    if str(contents[0]).endswith('added '):
-        added_tag = contents[1].text
-    elif str(contents[0]).endswith('removed '):
-        removed_tag = contents[1].text
-    else:
-        continue
+            contents = [c for c in activity_message_node.find('span').children]
 
-    if len(contents) == 5:
-        if str(contents[2]).endswith('added '):
-            added_tag = contents[3].text
-        elif str(contents[2]).endswith('removed '):
-            removed_tag = contents[3].text
-        else:
-            continue
+            is_tag_action = len(contents) in [3, 5]
+            if not is_tag_action:
+                continue
 
-    activity_time_text = activity.find('time').get('datetime')
-    activity_time_gmt_0 = date_parser.parse(activity_time_text)
-    activity_time = activity_time_gmt_0 + date_delta(hours=-3)
-    formatted_time = activity_time.strftime('%d/%m/%Y %H:%M')
+            if str(contents[0]).endswith('added '):
+                added_tag = contents[1].text
+            elif str(contents[0]).endswith('removed '):
+                removed_tag = contents[1].text
+            else:
+                continue
 
-    if added_tag in stage_tags:
-        stage_updates.append((added_tag, formatted_time))
-    # print(f'-{removed_tag}a', '\t\t', f'+{added_tag}', '  \tat', formatted_time)
+            if len(contents) == 5:
+                if str(contents[2]).endswith('added '):
+                    added_tag = contents[3].text
+                elif str(contents[2]).endswith('removed '):
+                    removed_tag = contents[3].text
+                else:
+                    continue
 
-stage_names, update_times = zip(*stage_updates)
+            activity_time_text = activity.find('time').get('datetime')
+            activity_time_gmt_0 = date_parser.parse(activity_time_text)
+            activity_time = activity_time_gmt_0 + date_delta(hours=-3)
+            formatted_time = activity_time.strftime('%d/%m/%Y %H:%M')
 
-print('')
-print('\t'.join(stage_names))
-print('\t'.join(update_times))
+            if added_tag in stage_tags:
+                self.stage_updates.append((added_tag, formatted_time))
+
+            # print(f'-{removed_tag}a', '\t\t',
+            #       f'+{added_tag}', '  \tat',
+            #       formatted_time)
+
+        html_file.close()
+
+        pass
+
+    def print_metrics(self):
+        stage_names, update_times = zip(*self.stage_updates)
+        print('')
+        print('\t'.join(stage_names))
+        print('\t'.join(update_times))
+
+
+spider = TaskSpider(2974)
+spider.scrap_metrics()
+spider.print_metrics()
