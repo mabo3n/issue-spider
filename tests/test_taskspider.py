77e8c68b4323e668bb4af361ee300389227c08dc
Marcel <dolly.marcel@gmail.com>
2020-12-18 14:47:34 -0300

    Create tests/fixtures and read task files from there

--- tests/test_taskspider.py
+++ tests/test_taskspider.py
@@ -1,12 +1,22 @@
 from taskspider import TaskSpider
+from os import path
+
+
+current_dir = path.dirname(path.abspath(__file__))
+fixtures_dir = path.join(current_dir, 'fixtures')
+
+
+def get_task_html(task_number):
+    task_file_name = f'{task_number}.html'
+    task_file_path = path.join(fixtures_dir, task_file_name)
+
+    with open(task_file_path, 'r') as task_file:
+        return task_file.read()
 
 
 def test_scrap_metrics():
-    task_number = 2974
-    task_file_path = f'/home/marcelbornancin/Downloads/{task_number}.html'
-
-    with open(task_file_path, 'r') as task_file:
-        spider = TaskSpider(task_file)
-        metrics = spider.scrap_metrics()
+    html = get_task_html(2974)
+    spider = TaskSpider(html)
+    metrics = spider.scrap_metrics()
 
     assert len(metrics) == 15
