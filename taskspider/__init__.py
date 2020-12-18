dde57d3fa89eaa81c567ebfe53f93c3548e9575b
Marcel <dolly.marcel@gmail.com>
2020-12-18 13:22:32 -0300

    Add sample test file

---
+++ taskspider/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['TaskSpider']
+
+from .spider import TaskSpider
--- tests/test_taskspider.py
+++ tests/test_taskspider.py
@@ -1,1 +1,12 @@
-import pytest, taskspider
+from taskspider import TaskSpider
+
+
+def test_scrap_metrics():
+    task_number = 2974
+    task_file_path = f'/home/marcelbornancin/Downloads/{task_number}.html'
+
+    with open(task_file_path, 'r') as task_file:
+        spider = TaskSpider(task_file)
+        metrics = spider.scrap_metrics()
+
+    assert len(metrics) == 15
